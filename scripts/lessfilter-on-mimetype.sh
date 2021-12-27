7a5f3717eab3d5a31f02cab373a7be1a182a169b
AlexisWilke <alexis@m2osw.com>
2021-12-26 21:56:08 -0800

    Force dereference on `file ...`

--- scripts/lessfilter-on-mimetype.sh
+++ scripts/lessfilter-on-mimetype.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-MIME=`file --brief --mime-type "${1}"`
+MIME=`file --brief --mime-type --dereference "${1}"`
 case "${MIME}" in
 "application/x-sharedlib"|"application/x-pie-executable")
         objdump -x "${1}"
