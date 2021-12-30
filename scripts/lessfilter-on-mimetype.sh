a60ee515df4a2a6d3687026856b54dd9872b95ea
AlexisWilke <alexis@m2osw.com>
2021-12-30 14:33:12 -0800

    Added another type for the .o files.

--- scripts/lessfilter-on-mimetype.sh
+++ scripts/lessfilter-on-mimetype.sh
@@ -2,7 +2,7 @@
 
 MIME=`file --brief --mime-type --dereference "${1}"`
 case "${MIME}" in
-"application/x-sharedlib"|"application/x-pie-executable")
+"application/x-sharedlib"|"application/x-pie-executable"|"application/x-object")
         objdump -x "${1}"
         exit 0
         ;;
