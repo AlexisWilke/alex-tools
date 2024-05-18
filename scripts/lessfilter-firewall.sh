1873222698a5c7f8398fe6d56745aa86f6bced77
AlexisWilke <alexis@m2osw.com>
2024-05-18 07:23:36 -0700

    Adding script to restart pulseaudio.
    On my Ubuntu 22.04, pulseaudio is not auto-restarted after a reboot.
    The script makes sure to stop everything and then restarts the daemon.
    
    The restart-system-monitor-extension is for Gnome users. Once in a while
    (really rarely for me), the app. crashes and thus does not give me feedback
    anymore. This script is useful to restart the app. without having to
    remember all the commands.

--- scripts/lessfilter-firewall.sh
+++ scripts/lessfilter-firewall.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -e
 
 # This is a cool feature but at this point I'd rather use my showfw script
-# (i.e. less /etc/network/firewall would output iptables -L ...)
-# Also this requires us to have an actual file named "firewall" somewhere
+# See: https://github.com/m2osw/iplock/blob/main/scripts/showfw
+#
+# Also this extension requires us to have an actual file named "firewall"
+#
+#     sudo touch /etc/network/firewall
+#     less /etc/network/firewall
 #
 #if test "`basename $1`" = "firewall"
 #then
